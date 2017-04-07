6203d0ca8d9d8e5765f566db922e5d24ef6bbb99
ReymanDJC <ReymanDJC@REYMANDJC-PC>
2017-04-07 02:00:14 -0400

    Log in Funcionando

--- LibreriaCamilo/Login.Designer.cs
+++ LibreriaCamilo/Login.Designer.cs
@@ -55,7 +55,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 151);
+            this.label3.Location = new System.Drawing.Point(12, 148);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 16);
             this.label3.TabIndex = 70;
@@ -65,7 +65,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 123);
+            this.label2.Location = new System.Drawing.Point(12, 120);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 16);
             this.label2.TabIndex = 69;
@@ -73,7 +73,7 @@
             // 
             // NombretextBox
             // 
-            this.NombretextBox.Location = new System.Drawing.Point(100, 119);
+            this.NombretextBox.Location = new System.Drawing.Point(93, 116);
             this.NombretextBox.MaxLength = 80;
             this.NombretextBox.Name = "NombretextBox";
             this.NombretextBox.Size = new System.Drawing.Size(197, 20);
@@ -81,7 +81,7 @@
             // 
             // ClavetextBox
             // 
-            this.ClavetextBox.Location = new System.Drawing.Point(100, 151);
+            this.ClavetextBox.Location = new System.Drawing.Point(93, 148);
             this.ClavetextBox.MaxLength = 20;
             this.ClavetextBox.Name = "ClavetextBox";
             this.ClavetextBox.PasswordChar = '*';
@@ -91,11 +91,11 @@
             // Entrarbutton
             // 
             this.Entrarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Entrarbutton.Location = new System.Drawing.Point(100, 193);
+            this.Entrarbutton.Image = ((System.Drawing.Image)(resources.GetObject("Entrarbutton.Image")));
+            this.Entrarbutton.Location = new System.Drawing.Point(114, 176);
             this.Entrarbutton.Name = "Entrarbutton";
-            this.Entrarbutton.Size = new System.Drawing.Size(88, 30);
+            this.Entrarbutton.Size = new System.Drawing.Size(64, 53);
             this.Entrarbutton.TabIndex = 2;
-            this.Entrarbutton.Text = "Entrar";
             this.Entrarbutton.UseVisualStyleBackColor = true;
             this.Entrarbutton.Click += new System.EventHandler(this.Entrarbutton_Click);
             // 
@@ -106,11 +106,11 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(209, 193);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(208, 176);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 30);
+            this.button1.Size = new System.Drawing.Size(64, 53);
             this.button1.TabIndex = 71;
-            this.button1.Text = "Salir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -118,7 +118,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(309, 235);
+            this.ClientSize = new System.Drawing.Size(308, 241);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Entrarbutton);
             this.Controls.Add(this.label3);
